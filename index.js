3b12f6b6d12aa7cc0f58a27ad458b5aee17fcdb6
Catherine Li <catherine.uk.20@gmail.com>
2021-05-04 17:44:46 +0100

    complete challenge 1

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ function createRandomNumber (numberOfNumber){
     return randomNumber
 }
 
+
 function getAnswer(){
-    userAnswer = prompt(`Enter the numbers in order now:`).split(",");
+
+    for(i = 1; i <= randomNumberArray.length; i++){
+    userAnswer.push(prompt(`Enter the ${ordinal(i)} numbers in order now:`));
+    }
     userAnswer = userAnswer.map(Number)
     for(i = 0; i <= randomNumberArray.length; i++ ){
         if (i === randomNumberArray.length) alert (`Brilliant!! You got it!!`)
@@ -23,6 +27,21 @@ function getAnswer(){
 
     }
 }
+
+function ordinal(number) {
+    const ordinalRules = new Intl.PluralRules("en-US", {type: "ordinal"});
+    const suffixes = {
+        one: "st",
+        two: "nd",
+        few: "rd",
+        other: "th"
+    };
+
+    const suffix = suffixes[ordinalRules.select(number)];
+    return (number + suffix);
+}
+
+
 
 // RUN THE GAME 
 let randomNumberArray = createRandomNumber(4)
